fd99fcea7d757d150f065a7f81cfccc1a3947959
Robin Simonot <simonot.robin@gmail.com>
2023-06-06 13:47:18 +0200

    Init commit => Lots of config => Basig home page that redirects to shop/coffee

---
+++ prisma/migrations/20230606110805_init/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "CoffeeItem" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "price" REAL NOT NULL,
+    "updatedAt" DATETIME NOT NULL
+);
---
+++ src/app/coffee/page.tsx
@@ -0,0 +1,10 @@
+export default function Coffee() {
+  return (
+    <>
+      <header>
+        <h1 className="mb-4 p-3 w-100 shadow-sm">Coffee</h1>
+      </header>
+      <div>List of drinks</div>
+    </>
+  );
+}
